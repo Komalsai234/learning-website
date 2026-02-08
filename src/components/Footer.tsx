60b5afca932997ff225c44fcf942bbb01707f719
Komalsai234 <99163734+Komalsai234@users.noreply.github.com>
2026-02-08 14:24:28 +0530

    simple-app

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="py-8 text-center">
-      <p className="text-sm text-[#8c7a6a]">
-        Made by Komal for Akka
-      </p>
-    </footer>
-  );
-}
